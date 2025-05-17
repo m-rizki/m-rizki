2d6ffd223e53a8fc4167bddc78ca52bdc17e8e5c
m-rizki <37399436+m-rizki@users.noreply.github.com>
2025-05-17 20:26:54 +0700

    Add footer

---
+++ app/components/footer/footer.tsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router";
+
+export default function Footer() {
+  return (
+    <footer className="footer sm:footer-horizontal border-t border-base-content/10 text-base-content items-center mt-24 py-12 px-8 sm:px-16 lg:px-32 xl:px-64">
+      <aside className="grid-flow-col items-center">
+        <nav className="grid grid-flow-col gap-4">
+          <Link to={"/"} className="link link-hover">
+            Home
+          </Link>
+          <Link to={"/about"} className="link link-hover">
+            About
+          </Link>
+          <Link to={"/projects"} className="link link-hover">
+            Projects
+          </Link>
+          <Link to={"/articles"} className="link link-hover">
+            Articles
+          </Link>
+        </nav>
+      </aside>
+      <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+        <p className="text-base-content/60">
+          © {2025} Muhamad Rizki -{" "}
+          <a className="link link-hover" href="/web_client_licenses.txt">
+            GNU General Public License v3.0
+          </a>
+        </p>
+      </nav>
+    </footer>
+  );
+}
--- app/components/layouts/site-layout.tsx
+++ app/components/layouts/site-layout.tsx
@@ -1,17 +1,17 @@
 import { Outlet } from "react-router";
 import Navbar from "../navbar/navbar";
+import Footer from "../footer/footer";
 
 export default function SiteLayout() {
   return (
     <>
       <Navbar />
-
-      <div className="px-8 sm:px-16 lg:px-32 xl:px-64">
+      <div className="px-8 sm:px-16 lg:px-32 xl:px-64 min-h-screen">
         <main>
           <Outlet />
         </main>
       </div>
-      {/* TODO : ADD FOOTER */}
+      <Footer />
     </>
   );
 }
