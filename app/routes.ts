08007e633b89bb781185f1e71c30e1a276022ac3
m-rizki <37399436+m-rizki@users.noreply.github.com>
2025-07-20 19:39:51 +0700

    fix error : /.well-known/appspecific/com.chrome.devtools.json
    that's a chromium dev tool thing where it lets you edit your source code directly in the browser.
    you can turn that feature off in your chrome settings chrome://flags. Disable "DevTools Project Settings"

--- app/routes.ts
+++ app/routes.ts
@@ -20,10 +20,4 @@ export default [
       ...articleItemRoutes,
     ]),
   ]),
-
-  // issue (temporary solution) : https://github.com/remix-run/react-router/issues/13516
-  route(
-    "/.well-known/appspecific/com.chrome.devtools.json",
-    "./routes/temporary/issue1.tsx",
-  ),
 ] satisfies RouteConfig;
