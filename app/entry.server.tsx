cbc5dc07c3dea85720f8f9030e86b281f45b8cd4
m-rizki <37399436+m-rizki@users.noreply.github.com>
2025-08-05 09:06:26 +0700

    update entry server

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -19,31 +19,26 @@ export default function handleRequest(
   responseStatusCode: number,
   responseHeaders: Headers,
   entryContext: EntryContext,
-  // loadContext: AppLoadContext,
-  // If you have middleware enabled:
-  // loadContext: unstable_entryContextProvider,
   routerContext: unstable_RouterContextProvider,
 ) {
   return new Promise((resolve, reject) => {
     let shellRendered = false;
     let userAgent = request.headers.get("user-agent");
 
-    // Ensure requests from bots and SPA Mode renders wait for all content to load before responding
-    // https://react.dev/reference/react-dom/server/renderToPipeableStream#waiting-for-all-content-to-load-for-crawlers-and-static-generation
     let readyOption: keyof RenderToPipeableStreamOptions =
       (userAgent && isbot(userAgent)) || entryContext.isSpaMode
         ? "onAllReady"
         : "onShellReady";
 
-    const { pipe, abort } = renderToPipeableStream(
+    let { pipe, abort } = renderToPipeableStream(
       <I18nextProvider i18n={getInstance(routerContext)}>
         <ServerRouter context={entryContext} url={request.url} />
       </I18nextProvider>,
       {
         [readyOption]() {
           shellRendered = true;
-          const body = new PassThrough();
-          const stream = createReadableStreamFromReadable(body);
+          let body = new PassThrough();
+          let stream = createReadableStreamFromReadable(body);
 
           responseHeaders.set("Content-Type", "text/html");
 
@@ -61,18 +56,11 @@ export default function handleRequest(
         },
         onError(error: unknown) {
           responseStatusCode = 500;
-          // Log streaming rendering errors from inside the shell.  Don't log
-          // errors encountered during initial shell rendering since they'll
-          // reject and get logged in handleDocumentRequest.
-          if (shellRendered) {
-            console.error(error);
-          }
+          if (shellRendered) console.error(error);
         },
       },
     );
 
-    // Abort the rendering stream after the `streamTimeout` so it has time to
-    // flush down the rejected boundaries
     setTimeout(abort, streamTimeout + 1000);
   });
 }
